981b397808b58b38c3852745211d26f0fd44e4b9
Jason Washburn <jason.washburn@gmail.com>
2023-02-05 12:34:44 -0600

    adds alt terminal and initial gameloop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use std::error::Error;
-
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{event, terminal, ExecutableCommand};
 use rusty_audio::Audio;
+use std::error::Error;
+use std::io;
+use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -10,8 +15,33 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("pew", "audio/pew.wav");
     audio.add("startup", "audio/startup.wav");
     audio.add("win", "audio/win.wav");
-
     audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    //Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
